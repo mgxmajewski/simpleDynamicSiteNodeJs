a6c47ea1ec30f89cfb82dc051f9f9ee9ff125c87
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-29 01:22:00 +0100

    Creates a Simple Server Node.js example code

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 //1. Create a web server
     //response.end('Hello World/n');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 //2. Handle HTTP route GET / and POST / i.e. Home
     //if url == "/" && GET
