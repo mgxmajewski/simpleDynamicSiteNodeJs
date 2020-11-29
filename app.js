b5cdf506f6c2214e78f1609068c0283dabe666a8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-29 01:55:48 +0100

    Fixes localhost issue

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(new Date() + "\n")
+    res.end('Hello World, I love you');
 });
 
 server.listen(port, hostname, () => {
