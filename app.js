8a9188c321fb857cd4a3a266843be9324fba7681
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-29 17:37:15 +0100

    Creates logic for homeRoute

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ server.listen(port, hostname, () => {
 //2. Handle HTTP route GET / and POST / i.e. Home
 function homeRoute(req, res) {
     //if url == "/" && GET
+    if (req.url === "/") {
         //show search
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.write("Header\n");
         res.write("Search\n");
         res.end("Footer\n");
-
+}
     //if url == "/" && POST
         //redirect to /:username
 }
