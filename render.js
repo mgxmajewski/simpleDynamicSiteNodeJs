adafece1bbb6946751977ca256287df560e49f37
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-29 22:52:07 +0100

    Adds mergeValues() to render.js

--- render.js
+++ render.js
@@ -1,10 +1,20 @@
 const fs = require("fs");
 
+function mergeValues(values, content) {
+    //Cycle over the keys
+    for (let key in values) {
+        //Replace all{{keys}} with the values from the values object
+        content = content.replace("{{" + key + "}}", values[key]);
+    }
+    //return merged content
+    return content;
+}
+
 function view(templateName, values, res) {
     //Read from template file
-    const fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    let fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
     //Insert values in to the content
-
+    fileContents = mergeValues(values, fileContents);
     //Write out the contents to the response
     res.write(fileContents);
 }
